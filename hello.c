8da67e203e9ae4842e14eb744f6288d5ef36f396
20251814 <chji0911f@naver.com>
2025-09-09 16:54:10 +0900

    finish

---
+++ hello.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int age = 45;
+    char grade = 'A';
+    char name[] = "Aaron";
+
+    printf("Hello World!~\n");
+    printf("My name is %s.\n", name);
+    printf("I'm %d years old.\n", age);
+    printf("\tI want a %c grade.\n", grade);
+    return 0;
+}
